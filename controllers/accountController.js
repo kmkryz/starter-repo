9dc939db7c8706f526fd823d835c4f79031c4eeb
kmkryz <kmkryz@gmail.com>
2024-06-28 10:01:04 -0400

    implemented error changes

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -7,16 +7,14 @@ require("dotenv").config()
 const accountController = {};
 
 accountController.getLoginPage = async function (req, res, next) {
-  try {
+  
     let nav = await utilities.getNav();
     res.render("account/login", { 
       title: "Login",
       nav,
-      messages: req.flash('notice')
+      errors: null,
     });
-  } catch (error) {
-    next(error);
-  }
+   
 };
 
 
@@ -24,17 +22,15 @@ accountController.getLoginPage = async function (req, res, next) {
  *  Get Registration Page
  * ************************** */
 accountController.getRegisterPage = async function (req, res, next) { 
-  try {
+ 
     let nav = await utilities.getNav();
     res.render("account/register", {
       title: "Register",
       nav,
-      messages: req.flash('notice')
+      errors: null,
       
     });
-  } catch (error) {
-    next(error);
-  }
+   
 };
 
 /* ****************************************
@@ -47,20 +43,19 @@ accountController.registerAccount = async function (req, res) {
   // Hash the password before storing
   let hashedPassword;
   try {
-    // regular password and cost (salt is generated automatically)
+    
     hashedPassword = await bcrypt.hashSync(account_password, 10);
   } catch (error) {
     req.flash("notice", 'Sorry, there was an error processing the registration.');
     res.status(500).render("account/register", {
       title: "Registration",
       nav,
-      messages: req.flash('notice'),
+      errors: null,
       locals: { account_firstname, account_lastname, account_email }
     });
     return;
   }
 
-  try {
     const regResult = await accountModel.registerAccount(
       account_firstname,
       account_lastname,
@@ -76,26 +71,26 @@ accountController.registerAccount = async function (req, res) {
       res.status(201).render("account/login", {
         title: "Login",
         nav,
-        messages: req.flash('notice')
+        errors: null,
       });
     } else {
       req.flash("notice", "Sorry, the registration failed.");
       res.status(501).render("account/register", {
         title: "Registration",
         nav,
-        messages: req.flash('notice'),
+        errors: null,
         locals: { account_firstname, account_lastname, account_email }
       });
     }
-  } catch (error) {
-    req.flash("notice", "Sorry, the registration failed.");
-    res.status(501).render("account/register", {
-      title: "Registration",
-      nav,
-      messages: req.flash('notice'),
-      locals: { account_firstname, account_lastname, account_email }
-    });
-  }
+  // } catch (error) {
+  //   req.flash("notice", "Sorry, the registration failed.");
+  //   res.status(501).render("account/register", {
+  //     title: "Registration",
+  //     nav,
+  //     messages: req.flash('notice'),
+  //     locals: { account_firstname, account_lastname, account_email }
+  //   });
+  // }
 };
 
 
@@ -103,49 +98,34 @@ accountController.registerAccount = async function (req, res) {
  *  Process login request
  * ************************************ */
 accountController.accountLogin = async function (req, res) {
-  let nav = await utilities.getNav();
-  const { account_email, account_password } = req.body;
-  const accountData = await accountModel.getAccountByEmail(account_email);
+  let nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  const accountData = await accountModel.getAccountByEmail(account_email)
   if (!accountData) {
-    req.flash("notice", "Please check your credentials and try again.");
-    res.status(400).render("account/login", {
-      title: "Login",
-      nav,
-      errors: null,
-      account_email,
-    });
-    return;
+   req.flash("notice", "Please check your credentials and try again.")
+   res.status(400).render("account/login", {
+    title: "Login",
+    nav,
+    errors: null,
+    account_email,
+   })
+  return
   }
   try {
-    if (await bcrypt.compare(account_password, accountData.account_password)) {
-      delete accountData.account_password;
-      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 });
-      if (process.env.NODE_ENV === 'development') {
-        res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 });
-      } else {
-        res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 });
-      }
-      req.flash("notice", `Welcome back, ${accountData.account_firstname}`);
-      return res.redirect("/account/");
-    } else {
-      req.flash("notice", "Please check your credentials and try again.");
-      res.status(400).render("account/login", {
-        title: "Login",
-        nav,
-        errors: null,
-        account_email,
-      });
-    }
+   if (await bcrypt.compare(account_password, accountData.account_password)) {
+   delete accountData.account_password
+   const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 })
+   if(process.env.NODE_ENV === 'development') {
+     res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+     } else {
+       res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
+     }
+   return res.redirect("/account/")
+   }
   } catch (error) {
-    req.flash("notice", "An error occurred during login. Please try again.");
-    res.status(500).render("account/login", {
-      title: "Login",
-      nav,
-      errors: null,
-      account_email,
-    });
+   return new Error('Access Forbidden')
   }
-};
+ }
 
 
 
@@ -153,16 +133,14 @@ accountController.accountLogin = async function (req, res) {
 *  Build Account Management View
 * *************************************** */
 accountController.buildAccountManagementView = async function (req, res, next) {
-  try {
+  
     let nav = await utilities.getNav();
     res.render("account/management", {
       title: "Account Management",
       nav,
-      messages: req.flash('notice')
+      errors: null,
     });
-  } catch (error) {
-    next(error);
-  }
+  
 };
 
 module.exports = accountController;
--- controllers/invController.js
+++ controllers/invController.js
@@ -4,7 +4,7 @@ const utilities = require("../utilities/");
 const invCont = {};
 
 invCont.buildByClassificationId = async function (req, res, next) {
-  try {
+  
     const classification_id = req.params.classificationId;
     const data = await invModel.getInventoryByClassificationId(classification_id);
     const grid = await utilities.buildClassificationGrid(data);
@@ -15,13 +15,11 @@ invCont.buildByClassificationId = async function (req, res, next) {
       nav,
       grid,
     });
-  } catch (error) {
-    next(error);
-  }
+  
 };
 
 invCont.buildByInventoryId = async function (req, res, next) {
-  try {
+  
     const inventory_id = req.params.inventoryId;
     const data = await invModel.getInventoryById(inventory_id);
     if (!data) {
@@ -35,41 +33,34 @@ invCont.buildByInventoryId = async function (req, res, next) {
       nav,
       detail,
     });
-  } catch (error) {
-    next(error);
-  }
+  
 };
 
 /* ***************************
  *  Build Inventory Management View
  * ************************** */
 invCont.buildManagementView = async function (req, res, next) {
-  try {
+  
     let nav = await utilities.getNav();
     res.render("inventory/management", {
       title: "Inventory Management",
       nav,
-      messages: req.flash('notice')
+      errors: null,
     });
-  } catch (error) {
-    next(error);
-  }
-};
+  } ;
 
 /* ***************************
  *  Get Add Classification Page
  * ************************** */
 invCont.getAddClassificationPage = async function (req, res, next) {
-  try {
+  
     let nav = await utilities.getNav();
     res.render("inventory/add-classification", {
       title: "Add Classification",
       nav,
-      messages: req.flash('notice')
+      errors: null,
     });
-  } catch (error) {
-    next(error);
-  }
+  
 };
 
 /* ***************************
@@ -79,7 +70,7 @@ invCont.addClassification = async function (req, res, next) {
   let nav = await utilities.getNav();
   const { classification_name } = req.body;
 
-  try {
+ 
     const addResult = await invModel.addClassification(classification_name);
 
     if (addResult.rowCount === 1) {
@@ -88,42 +79,32 @@ invCont.addClassification = async function (req, res, next) {
       res.status(201).render("inventory/management", {
         title: "Inventory Management",
         nav,
-        messages: req.flash('notice')
+        errors: null,
       });
     } else {
       req.flash("notice", "Failed to add classification.");
       res.status(500).render("inventory/add-classification", {
         title: "Add Classification",
         nav,
-        messages: req.flash('notice')
+        errors: null,
       });
-    }
-  } catch (error) {
-    req.flash("notice", "Failed to add classification.");
-    res.status(500).render("inventory/add-classification", {
-      title: "Add Classification",
-      nav,
-      messages: req.flash('notice')
-    });
-  }
+    
+  } 
 };
 
 /* ***************************
  *  Get Add Inventory Page
  * ************************** */
 invCont.getAddInventoryPage = async function (req, res, next) {
-  try {
+  
     let nav = await utilities.getNav();
     let classificationList = await utilities.buildClassificationList();
     res.render("inventory/add-inventory", {
       title: "Add New Vehicle",
       nav,
       classificationList,
-      messages: req.flash('notice')
+      errors: null,
     });
-  } catch (error) {
-    next(error);
-  }
 };
 
 /* ***************************
@@ -133,7 +114,7 @@ invCont.addInventory = async function (req, res, next) {
   let nav = await utilities.getNav();
   const { classification_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color } = req.body;
 
-  try {
+
     const addResult = await invModel.addInventory(
       classification_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color
     );
@@ -144,7 +125,7 @@ invCont.addInventory = async function (req, res, next) {
       res.status(201).render("inventory/management", {
         title: "Inventory Management",
         nav,
-        messages: req.flash('notice')
+        errors: null,
       });
     } else {
       req.flash("notice", "Failed to add vehicle.");
@@ -154,20 +135,10 @@ invCont.addInventory = async function (req, res, next) {
         nav,
         classificationList,
         inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color,
-        messages: req.flash('notice')
+        errors: null,
       });
-    }
-  } catch (error) {
-    req.flash("notice", "Failed to add vehicle.");
-    let classificationList = await utilities.buildClassificationList(classification_id);
-    res.status(500).render("inventory/add-inventory", {
-      title: "Add New Vehicle",
-      nav,
-      classificationList,
-      inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color,
-      messages: req.flash('notice')
-    });
-  }
+    
+  } 
 };
 
 module.exports = invCont;
